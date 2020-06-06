ce5d769ceb2e99429b50206b67e2d41d3295ff04
Luigi Santagada <luigi.santagada@live.com>
2020-06-06 12:28:36 +0200

    Modificata versione dopo il test

--- BasicPortableAlarmSystem/PortableAlarmSystem.ino
+++ BasicPortableAlarmSystem/PortableAlarmSystem.ino
@@ -15,7 +15,7 @@
 #include "MySim900.h"
 #include "ActivityManager.h"
 
-char version[15] = "-P01 2.11-beta";
+char version[15] = "S001 2.20-beta";
  
 ActivityManager* _delayForTemperature = new ActivityManager(60);
 
